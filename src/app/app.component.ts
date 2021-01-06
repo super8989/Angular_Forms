f5df45e88f62dcfd103759ce12ae4ce9ed34ada4
Sam <super8989@gmail.com>
2021-01-06 11:54:06 -0500

    TD Submitting and Using the Form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'forms';
+
+  onSubmit(form: NgForm) {
+    console.log(form);
+  }
 }
