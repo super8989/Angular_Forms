d5f7f9672308a635c9ed2d7e3048f6d5b603166b
Sam <super8989@gmail.com>
2021-01-06 13:57:13 -0500

    TD Using ngModel with Two-Way-Binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,6 +12,7 @@ export class AppComponent {
   @ViewChild('f') signupForm: NgForm;
   @ViewChild('email') emailForm: NgModel;
   defaultQuestion = 'pet';
+  answer: string = '';
 
   // onSubmit(form: NgForm) {
   //   console.log(form);
@@ -19,6 +20,7 @@ export class AppComponent {
 
   onSubmit() {
     console.log(this.signupForm);
-    console.log('emailform', this.emailForm);
+    // console.log(this.signupForm.value);
+    // console.log('emailform', this.emailForm);
   }
 }
